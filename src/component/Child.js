ccb544733ae8207bc5e5f971ecbdc2c62e4e00e5
fahad Mridha <sohelcse16@gmail.com>
2023-03-22 00:12:46 +0600

    counter  description added

--- src/component/Child.js
+++ src/component/Child.js
@@ -15,21 +15,6 @@ const Child = () => {
   return (
     <div className="text-center">
       <p>{state}</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam libero
-        explicabo cum quibusdam, voluptatibus omnis quas necessitatibus, rem
-        laborum assumenda earum provident quod molestias dignissimos ratione.
-        Fugiat, impedit itaque earum quae expedita voluptatem ab amet vitae vero
-        eum quo aut fuga cum debitis facere temporibus ut porro molestias sequi
-        atque reiciendis numquam natus. Sit fugit deserunt architecto odit magni
-        obcaecati ea illum cumque. Quos aperiam sed, doloremque, ullam suscipit
-        velit voluptatibus necessitatibus maxime eligendi earum ipsum animi
-        magnam a hic laudantium eveniet nemo facere ea alias aspernatur ex ut
-        minus. Consequuntur corporis omnis provident officiis, eaque voluptas
-        praesentium ullam cum beatae iste quos eos id soluta deleniti mollitia
-        quis, quisquam neque inventore alias. Ad atque molestiae consequatur
-        impedit dolor nam.
-      </p>
       <button
         onClick={() => dispatch({ type: "INCREMENT", payload: { count: 5 } })}
       >
